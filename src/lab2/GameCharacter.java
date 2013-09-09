3a43470d5e6b354c4ebd19f2ce02b2bf5dd0eecb
netzl_000 <netzl_000@10.30.16.195>
2013-09-08 23:45:55 -0500

    started the lab2

---
+++ src/lab2/GameCharacter.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author netzl_000
+ */
+public class GameCharacter {
+    private String playerOne;
+    private String palyerTwo;
+    private String playerThree;
+    private String palyerFour;
+
+    public String getPlayerOne() {
+        //needs validation
+        return playerOne;
+    }
+
+    public void setPlayerOne(String playerOne) {
+        //needs validation
+        this.playerOne = playerOne;
+    }
+
+    public String getPalyerTwo() {
+        //needs validation
+        return palyerTwo;
+    }
+
+    public void setPalyerTwo(String palyerTwo) {
+        //needs validation
+        this.palyerTwo = palyerTwo;
+    }
+
+    public String getPlayerThree() {
+        //needs validation
+        return playerThree;
+    }
+
+    public void setPlayerThree(String playerThree) {
+        //needs validation
+        this.playerThree = playerThree;
+    }
+
+    public String getPalyerFour() {
+        //needs validation
+        return palyerFour;
+    }
+
+    public void setPalyerFour(String palyerFour) {
+        //needs validation
+        this.palyerFour = palyerFour;
+    }
+    
+    
+}
